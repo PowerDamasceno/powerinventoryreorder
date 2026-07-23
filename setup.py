645a1dce44ae18ff6029c96dbd11df8bb67cab77
PowerDamasceno <gabriel.damasceno@powersoft.com>
2026-07-23 16:14:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="powerinventoryreorder",
-    version="1.5.0",
+    version="1.6.0",
     packages=find_packages(),
     entry_points={
         "inventree_plugins": [
