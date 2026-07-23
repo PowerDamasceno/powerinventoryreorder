720637adbe3acb85908bb44e50b8de4a55e61cf8
PowerDamasceno <gabriel.damasceno@powersoft.com>
2026-07-23 15:29:22 +0200

    Update plugin metadata in powerinventoryreorder

--- powerinventoryreorder/plugin.py
+++ powerinventoryreorder/plugin.py
@@ -7,7 +7,10 @@ class PowerInventoryReorderPlugin(SettingsMixin, InvenTreePlugin):
     NAME = "Power Inventory Reorder"
     SLUG = "powerinventoryreorder"
     TITLE = "Power Inventory Reorder"
+
     VERSION = "1.1.0"
+    AUTHOR = "Gabriel Damasceno"
+    DESCRIPTION = "Daily reorder report"
 
     SETTINGS = {
         "RECIPIENT_EMAIL": {
