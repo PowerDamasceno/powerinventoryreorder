751fa48ae9b4ac7577a8ec7562d034acccfe1fa7
PowerDamasceno <gabriel.damasceno@powersoft.com>
2026-07-23 14:59:14 +0200

    Create Power Inventory Reorder plugin

---
+++ powerinventoryreorder/plugin.py
@@ -0,0 +1,8 @@
+from plugin import InvenTreePlugin
+
+
+class PowerInventoryReorderPlugin(InvenTreePlugin):
+
+    NAME = "Power Inventory Reorder"
+    SLUG = "powerinventoryreorder"
+    TITLE = "Power Inventory Reorder"
