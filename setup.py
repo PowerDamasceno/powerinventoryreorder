4ac07f1927436ed47a6d6e76ca7cb3929e903a23
PowerDamasceno <gabriel.damasceno@powersoft.com>
2026-07-23 15:52:14 +0200

    Bump version from 1.3.0 to 1.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="powerinventoryreorder",
-    version="1.3.0",
+    version="1.4.0",
     packages=find_packages(),
     entry_points={
         "inventree_plugins": [
