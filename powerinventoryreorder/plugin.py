76e24edc6ca1b4a263049058818adc3167423166
PowerDamasceno <gabriel.damasceno@powersoft.com>
2026-07-23 15:22:44 +0200

    Update plugin.py

--- powerinventoryreorder/plugin.py
+++ powerinventoryreorder/plugin.py
@@ -1,8 +1,18 @@
 from plugin import InvenTreePlugin
+from plugin.mixins import SettingsMixin
 
 
-class PowerInventoryReorderPlugin(InvenTreePlugin):
+class PowerInventoryReorderPlugin(SettingsMixin, InvenTreePlugin):
 
     NAME = "Power Inventory Reorder"
     SLUG = "powerinventoryreorder"
     TITLE = "Power Inventory Reorder"
+    VERSION = "1.1.0"
+
+    SETTINGS = {
+        "RECIPIENT_EMAIL": {
+            "name": "Recipient Email",
+            "description": "Email recipient for reorder reports",
+            "default": "gabriel.damasceno@powersoft.com",
+        }
+    }
