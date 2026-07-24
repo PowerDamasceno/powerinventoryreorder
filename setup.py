622cdd285bc33ebfc252594461dce397af14dcb7
PowerDamasceno <gabriel.damasceno@powersoft.com>
2026-07-24 12:40:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="powerinventoryreorder",
-    version="2.1.3.4",
+    version="2.1.3.5",
     packages=find_packages(),
     include_package_data=True,
     package_data={
