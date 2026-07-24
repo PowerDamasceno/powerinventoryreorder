f4f472cde187903f808052f0ab313c3d36f22050
PowerDamasceno <gabriel.damasceno@powersoft.com>
2026-07-24 08:25:00 +0200

    Update ui_settings.js

--- powerinventoryreorder/static/ui_settings.js
+++ powerinventoryreorder/static/ui_settings.js
@@ -3,21 +3,25 @@ export function renderPluginSettings(target, data) {
     console.log("PowerInventoryReorder", data);
 
     target.innerHTML = `
-        <h3>Power Inventory Reorder</h3>
+        <div style="padding:20px">
 
-        <p>
-            <strong>Status:</strong>
-            ${data.context.status}
-        </p>
+            <h3>Power Inventory Reorder</h3>
 
-        <p>
-            <strong>Parts with IPN:</strong>
-            ${data.context.total_parts}
-        </p>
+            <p>
+                <strong>Status:</strong>
+                ${data.context.status}
+            </p>
 
-        <p>
-            <strong>Reorder candidates:</strong>
-            ${data.context.reorder_parts}
-        </p>
+            <p>
+                <strong>Parts with IPN:</strong>
+                ${data.context.total_parts}
+            </p>
+
+            <p>
+                <strong>Reorder candidates:</strong>
+                ${data.context.reorder_parts}
+            </p>
+
+        </div>
     `;
 }
