42c22d4fe82c17795af1e1a1c3a91b58a88df2a0
PowerDamasceno <gabriel.damasceno@powersoft.com>
2026-07-24 12:50:14 +0200

    Bump version from 2.1.3.5 to 2.1.3.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="powerinventoryreorder",
-    version="2.1.3.5",
+    version="2.1.3.6",
     packages=find_packages(),
     include_package_data=True,
     package_data={
