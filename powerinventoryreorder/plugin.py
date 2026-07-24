a088a94c76c534ea888aa99a9a7ae7ca23af1183
PowerDamasceno <gabriel.damasceno@powersoft.com>
2026-07-24 13:24:02 +0200

    Update plugin.py

--- powerinventoryreorder/plugin.py
+++ powerinventoryreorder/plugin.py
@@ -24,7 +24,7 @@ class PowerInventoryReorderPlugin(
     SLUG = "powerinventoryreorder"
     TITLE = "Power Inventory Reorder"
 
-    VERSION = "2.1.3.7"
+    VERSION = "2.1.3.8"
     AUTHOR = "Gabriel Damasceno"
     DESCRIPTION = "Daily reorder report"
 
